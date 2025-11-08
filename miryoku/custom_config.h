89533fcf254328ae453c87b3803ef2613b412052
Caio Peluti <cpeluti@gmail.com>
2025-11-08 14:36:35 -0300

    Fine tunning the gaming layout

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -19,16 +19,6 @@ U_NP,               U_NP,              &kp LALT,          &kp SPC,           &mo
 &kp Z,              &kp NUM_7,         &kp NUM_8,         &kp NUM_9,         &kp B,             &kp BSLH,          &kp F1,            &kp F2,            &kp F3,            &kp GRAVE,        \
 U_NP,               U_NP,              &to U_BASE,        &kp SPC,           &none,             &kp MINUS,         &kp NUM_0,         &kp DOT,           U_NP,              U_NP
 
-#define MIRYOKU_LAYERMAPPING_CORNE( \
-     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
-     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
-     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
-) \
-XXX                         K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &u_to_U_GAME \
-XXX                         K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-&mt LC(LS(LA(LGUI)))        K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &mt LC(LS(LA(LGUI))) \
-XXX                                   K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_LAYER_LIST \
 MIRYOKU_X(BASE,   "Base") \
@@ -68,5 +58,4 @@ MIRYOKU_X(GAMENUM,"GNum")
 #define U_GAME       10
 #define U_GAMENUM    11
 
-#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_CORNE
 #define MIRYOKU_LAYERMAPPING_GAMENUM MIRYOKU_MAPPING
