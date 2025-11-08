44a286eb9e466bc7a8e37c69c37399bf6ce8820b
Caio Peluti <cpeluti@gmail.com>
2025-11-08 12:56:27 -0300

    fixing custom config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -6,7 +6,7 @@
 
 #define XXX &none
 
-#define MIRYOKU_LAYERMAPPING_BASE( \
+#define MIRYOKU_LAYOUTMAPPING_CORNE( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
@@ -16,6 +16,19 @@ XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp ESC \
 XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
+
+#define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
+#define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTINNERINDEX 3 4
+#define MIRYOKU_KLUDGE_TOPROWCOMBOS_RIGHTINNERINDEX 7 8
+#define MIRYOKU_KLUDGE_TOPROWCOMBOS_RIGHTPINKIE 8 9
+
+#define MIRYOKU_KLUDGE_BOTTOMROWCOMBOS_LEFTPINKIE 26 27
+#define MIRYOKU_KLUDGE_BOTTOMROWCOMBOS_LEFTINNERINDEX 27 28
+#define MIRYOKU_KLUDGE_BOTTOMROWCOMBOS_RIGHTINNERINDEX 31 32
+#define MIRYOKU_KLUDGE_BOTTOMROWCOMBOS_RIGHTPINKIE 32 33
+
+#define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 37 38
+#define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 39 40
 
 #define MIRYOKU_LAYER_LIST \
 MIRYOKU_X(BASE,   "Base") \
@@ -29,13 +42,13 @@ MIRYOKU_X(NUM,    "Num") \
 MIRYOKU_X(SYM,    "Sym") \
 MIRYOKU_X(FUN,    "Fun")
 
-#define U_BASE        0
-#define U_EXTRA       1
-#define U_TAP         2
-#define U_BUTTON      3
-#define U_NAV         4
-#define U_MOUSE       5
-#define U_MEDIA       6
-#define U_NUM         7
-#define U_SYM         8
-#define U_FUN         9
+#define U_BASE   0
+#define U_EXTRA  1
+#define U_TAP    2
+#define U_BUTTON 3
+#define U_NAV    4
+#define U_MOUSE  5
+#define U_MEDIA  6
+#define U_NUM    7
+#define U_SYM    8
+#define U_FUN    9
