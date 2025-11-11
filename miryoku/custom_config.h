6f8bb7301eeb38aa67d7ab89140ab9081e6030fd
Caio Peluti <cpeluti@gmail.com>
2025-11-11 09:11:48 -0300

    reusing tap and extra as gaming

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -11,7 +11,7 @@
 &kp Q,              &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 &kp A,              &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             &kp J,             &kp K,             &kp L,             &kp SQT,           \
 &kp Z,              &kp X,             &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
-U_NP,               U_NP,              &kp LALT,          &kp SPC,           &mo U_GAMENUM,     &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
+U_NP,               U_NP,              &kp LALT,          &kp SPC,           &mo U_TAP,         &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
 
 #define MIRYOKU_LAYER_GAMENUM \
 &kp Q,              &kp NUM_1,         &kp NUM_2,         &kp NUM_3,         &kp T,             &kp LBKT,          &kp F7,            &kp F8,            &kp F9,            &kp RBKT,         \
@@ -25,7 +25,7 @@ U_NP,               U_NP,              &to U_BASE,        &kp SPC,           &no
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-XXX                         K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &u_to_U_GAME \
+XXX                         K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &u_to_U_EXTRA \
 XXX                         K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
 &mt LC(LS(LA(LGUI)))        K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &mt LC(LS(LA(LGUI))) \
 XXX                                   K32  K33  K34       K35  K36  K37
